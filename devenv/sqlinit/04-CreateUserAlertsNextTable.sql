c8b0c31feccbc28de1fd0745a8e53bc7f03408b9
karashiiro <49822414+karashiiro@users.noreply.github.com>
2023-01-25 10:05:23 -0800

    Create devenv

---
+++ devenv/sqlinit/04-CreateUserAlertsNextTable.sql
@@ -0,0 +1,12 @@
+USE `dalamud`;
+CREATE TABLE `users_alerts_next` (
+  `id` CHAR(36) NOT NULL,
+  `user_id` CHAR(36) DEFAULT NULL,
+  `item_id` INT NOT NULL,
+  `world_id` INT NOT NULL,
+  `discord_webhook` TEXT DEFAULT NULL,
+  `trigger` TEXT NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY (`item_id`, `world_id`),
+  CONSTRAINT `FK_user_id_users_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
